b3dfa1d22255f10d31e7f1f009bf666045aa28c7
Luka Brkovic <lucabrkovic@gmx.de>
2022-11-21 09:51:15 +0100

    2 Commit

--- minesweaper.js
+++ minesweaper.js
@@ -11,6 +11,7 @@ const TILE_STATUSES = {
 export function createBoard(boardSize, numberOfMines) {
     const board = []
     const minePositions = getMinePositions(boardSize, numberOfMines)
+    console.log(minePositions)
     for (let x = 0; x <boardSize; x++) {
         const row = []
         for(let y = 0; y <boardSize; y++){
@@ -21,10 +22,13 @@ export function createBoard(boardSize, numberOfMines) {
             element,
             x,
             y,
-            mine: true,
+            // check if one of them match current x,y cordionates
+            // The some() method tests whether at least one element in the array passes the test implemented by the provided function. It returns true if, in the array, it finds an element
+            mine: minePositions.some(positionMatch.bind(null, {x, y})),
             get status() {
                 return element.dataset.status
             },
+            // updating status, changing background color
             set status(value) {
                 this.element.dataset.status = value
             }
@@ -36,3 +40,45 @@ export function createBoard(boardSize, numberOfMines) {
     }
     return board
 }
+
+// some- if at least one of the elements match the search in array
+function getMinePositions(boardSize, numberOfMines) {
+    const positions = []
+
+    while (positions.length < numberOfMines) {
+      const position = {
+        x: randomNumber(boardSize),
+        y: randomNumber(boardSize),
+      }
+  // if they already exist we will skip adding them
+      if (!positions.some(positionMatch.bind(null, position))) {
+        positions.push(position)
+      }
+    }
+  
+    return positions
+}
+// checking if they have same cordinates 
+function positionMatch(a, b) {
+    return a.x === b.x && a.y === b.y
+  }
+  // generate random position
+  function randomNumber(size) {
+    return Math.floor(Math.random() * size)
+  }
+
+  // checking tile status (right click)
+ export function markTile(tile) {
+    if (
+      tile.status !== TILE_STATUSES.HIDDEN &&
+      tile.status !== TILE_STATUSES.MARKED
+    ) {
+      return
+    }
+  
+    if (tile.status === TILE_STATUSES.MARKED) {
+      tile.status = TILE_STATUSES.HIDDEN
+    } else {
+      tile.status = TILE_STATUSES.MARKED
+    }
+  }
--- script.js
+++ script.js
@@ -1,24 +1,39 @@
 // display/UI 
-import { createBoard } from "./minesweaper.js";
-
+import { createBoard, markTile } from "./minesweaper.js";
+// 1. Populate a board with mines 
 const BOARD_SIZE = 10
 const NUMBER_OF_MINES = 2
 
 
 const board = (createBoard(BOARD_SIZE, NUMBER_OF_MINES))
 const boardElement  = document.querySelector(".board")
+const minesLeftText = document.querySelector("[data-mine-count]")
+
+
+
 console.log(board)
+
 board.forEach(row => {
     row.forEach(tile => {
         boardElement.append(tile.element)
+        //2 for left clicks
+        tile.element.addEventListener("click", () => {
+
+        })
+        //3 for right clicks
+        // preventDefault() prevent default behavior of the right click
+        tile.element.addEventListener("contextmenu", e => {
+            e.preventDefault()
+            markTile(tile)
+        })
     })
 })
 boardElement.style.setProperty("--size", BOARD_SIZE)
+minesLeftText.textContent = NUMBER_OF_MINES
 
-// 1. Populate a board with mines 
-// 2. left click on tiles 
-    // reavel tiles
-// 3. right click on tiles
-    // mark tiles
+
+    // a. reavel tiles
+
+    // b. mark tiles
 // 4. check for win/lose 
 
